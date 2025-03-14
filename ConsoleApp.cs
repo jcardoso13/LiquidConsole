4cebae5104e9d47883b1d3536a7fea3cec937ff6
João Cardoso <jpcardoso@outlook.pt>
2025-03-14 10:23:38 +0000

    changes to adapt to fluid

--- ConsoleApp.cs
+++ ConsoleApp.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Text.Encodings.Web;
 
 var serilogLogger = new LoggerConfiguration()
     .Enrich.FromLogContext()
@@ -66,17 +67,18 @@ microsoftLogger.LogInformation($"Running Liquid Processor took {stopwatch.Elapse
 // Writing output
 stopwatch.Restart();
 split = args[2].Split(".");
-microsoftLogger.LogInformation("Content Type is " + split[2]);
-switch (split[2])
+microsoftLogger.LogInformation("Content Type is " + split.Last());
+switch (split.Last())
 {
     case "json": 
     contenttype = "application/json"; 
     var jsonElement = JsonSerializer.Deserialize<Dictionary<string, object>>(result.Output, new JsonSerializerOptions
             {
                 Converters = {new DictionaryStringObjectJsonConverter()},
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
                 AllowTrailingCommas = true // Allow trailing commas
             });
-    result.Output = JsonSerializer.Serialize(jsonElement, new JsonSerializerOptions { WriteIndented = true });
+    result.Output = JsonSerializer.Serialize(jsonElement, new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping, WriteIndented = true, AllowTrailingCommas = true});
     break;
     case "xml": contenttype = "application/xml"; break;
     case "csv": contenttype = "text/csv"; break;
